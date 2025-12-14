54c988d5e29936dfe38fed008e9f87711e78c140
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-13 23:57:32 -0800

    starting to implement Plan class

---
+++ Recuria/Recuria.Domain/Plan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain
+{
+    public enum PlanType
+    {
+        Free,
+        Pro,
+        Enterprise
+    }
+
+    public class Plan
+    {
+        public PlanType Type { get; private set; }
+        public int MaxUsers { get; private set; }
+        public decimal MonthlyPrice { get; private set; }
+    }
+}
