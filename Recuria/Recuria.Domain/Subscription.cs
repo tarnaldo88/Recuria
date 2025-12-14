b7fbfb064fc33eb103142a66220501e9b4af1bff
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-14 00:03:28 -0800

    completed constructor and class members

--- Recuria/Recuria.Domain/Subscription.cs
+++ Recuria/Recuria.Domain/Subscription.cs
@@ -19,5 +19,22 @@ namespace Recuria.Domain
     {
         public Guid Id { get; init; }
         public Guid OrganizationId { get; private set; }
+        public Organization Organization_ { get; private set; }
+
+        public PlanType Plan_ {  get; private set; }
+        public SubscriptionStatus Status { get; private set; }
+        public DateTime PeriodStart { get; private set; }
+        public DateTime PeriodEnd { get; private set; }
+
+        public Subscription(Guid id, PlanType plan)
+        {
+            OrganizationId = id;
+            Plan_ = plan;
+            Status = SubscriptionStatus.Trialing;
+            PeriodStart = DateTime.UtcNow;
+            PeriodEnd = DateTime.UtcNow.AddDays(14);
+        }
+
+
     }
 }
