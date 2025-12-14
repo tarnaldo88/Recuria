57abd2c7e00de2fab8da815f9c17ff919767f621
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-13 23:51:16 -0800

    Adding and beginning implementation of Organization and User Classes

---
+++ Recuria/Recuria.Domain/Organization.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain
+{
+    //internal class Organization
+    //{
+    //}
+
+    public class Organization
+    {
+        public Guid Id { get; set; }
+        public string Name { get; private set; } = null!;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+
+        public List<User> Users { get; private set; } = new();
+
+    }
+
+}
