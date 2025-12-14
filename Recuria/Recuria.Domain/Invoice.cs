c652bcf17ee2f1c0ebcbe38cdcdcfad12e3224ab
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-14 00:07:46 -0800

    starting to implement Invoice class

---
+++ Recuria/Recuria.Domain/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain
+{
+    public class Invoice
+    {
+        public Guid Id { get; init; }
+        public Guid SubscriptionId { get; private set; }
+
+    }
+}
