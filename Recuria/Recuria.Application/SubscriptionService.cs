711b6a8a148efe026c93a803972b3e4eb74280f9
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-14 16:11:52 -0800

    Created Subscription Service that uses sub service interface, implementing member functions

---
+++ Recuria/Recuria.Application/SubscriptionService.cs
@@ -0,0 +1,21 @@
+﻿using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Recuria.Application
+{
+    public class SubscriptionService : ISubscriptionService
+    {
+        public Subscription CreateTrial(Organization org)
+        {
+            var subscription = new Subscription(org.Id, PlanType.Free);
+            subscription.Activate(DateTime.UtcNow);
+            org.AssignSubscription(subscription);
+            return subscription;
+        }
+    }
+}
