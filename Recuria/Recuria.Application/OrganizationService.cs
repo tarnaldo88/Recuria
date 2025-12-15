3993829882143239cac39e701c6e33a0cd19f4d1
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-14 20:20:46 -0800

    Implemented create org method and AddUser method

---
+++ Recuria/Recuria.Application/OrganizationService.cs
@@ -0,0 +1,32 @@
+﻿using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public class OrganizationService : IOrganizationService
+    {
+        Organization CreateOrganization(string name, User owner)
+        {
+            Organization newOrg = new Organization(name);
+            newOrg.Users.Add(owner);
+            return newOrg;
+        }
+
+        void AddUser(Organization organization, User user, UserRole role)
+        {
+            var newUser = new User(user.Email, user.Name, user.Role, organization);
+            organization.Users.Add(newUser);
+        }
+
+        void ChangeUserRole(Organization organization, Guid userId, UserRole role)
+        {
+
+        }
+
+        void RemoveUser(Organization organization, Guid userId);
+    }
+}
