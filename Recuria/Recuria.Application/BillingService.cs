59fb3c2418b8fefe8fa7e10f973bc096428f6fdb
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 10:31:55 -0800

    Starting to Implement billing service class

---
+++ Recuria/Recuria.Application/BillingService.cs
@@ -0,0 +1,24 @@
+﻿using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public class BillingService : IBillingService
+    {
+        private static readonly int GracePeriodDays = 7;
+
+        Invoice RunBillingCycle(Subscription subscription, DateTime now)
+        {
+            var inv = new Invoice(subscription.Id, subscription.Plan_.);
+        }
+
+        void HandleOverdueSubscription(Subscription subscription, DateTime now)
+        {
+
+        }
+    }
+}
