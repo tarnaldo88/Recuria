20cf7412ab6d62f0293677acf1092aad45d9d156
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 17:16:46 -0800

    Created Db context for Recuria

---
+++ Recuria/Recuria.Infrastructure/Persistence/RecuriaDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recuria.Application;
+
+namespace Recuria.Infrastructure.Persistence
+{
+    public class RecuriaDbContext : DbContext
+    {
+        public RecuriaDbContext(DbContextOptions<RecuriaDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Organization> Organizations => Set<Organization>();
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Subscription> Subscriptions => Set<Subscription>();
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(
+                typeof(RecuriaDbContext).Assembly
+            );
+        }
+    }
+}
