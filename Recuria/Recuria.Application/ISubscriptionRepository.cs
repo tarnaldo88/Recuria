e797abca8abc9e1e43f2a3696e602b1fffed398a
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 16:40:43 -0800

    Created Subscription repository interface

---
+++ Recuria/Recuria.Application/ISubscriptionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public interface ISubscriptionRepository
+    {
+        Task<Subscription> GetByOrganizationIdAsync(Guid organizationId);
+
+        Task<Subscription> GetByIdAsync(Guid id);
+        Task AddAsync(Subscription subscription);
+        Task UpdateAsync(Subscription subscription);
+    }
+}
