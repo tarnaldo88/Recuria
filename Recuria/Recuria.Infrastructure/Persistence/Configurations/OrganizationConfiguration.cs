476cb831856d8aa7e0de82bea5e3d4e34ccf7434
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 17:23:38 -0800

    created Org config

---
+++ Recuria/Recuria.Infrastructure/Persistence/Configurations/OrganizationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Persistence.Configurations
+{
+    public class OrganizationConfiguration : IEntityTypeConfiguration<Organization>
+    {
+        public void Configure(EntityTypeBuilder<Organization> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            builder.Property(o => o.Name).IsRequired().HasMaxLength(200);
+
+            builder.HasMany(o => o.Users).WithOne(u => u.Organization!).HasForeignKey(u => u.Organization.Id);
+
+            builder.HasOne(o => o.CurrentSubscription).WithOne().HasForeignKey<Subscription>(s => s.OrganizationId);
+        }
+    }
+}
