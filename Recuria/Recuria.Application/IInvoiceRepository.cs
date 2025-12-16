675a5daef0f66e1ef3e1c4f29ebadc13c474d69f
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 16:43:02 -0800

    Created Invoice Repo interface

---
+++ Recuria/Recuria.Application/IInvoiceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Recuria.Domain;
+
+namespace Recuria.Application
+{
+    public interface IInvoiceRepository
+    {
+        Task AddAsync(Invoice invoice);
+        Task<IReadOnlyList<Invoice>> GetBySubscriptionIdAsync(Guid subscriptionId);
+    }
+}
