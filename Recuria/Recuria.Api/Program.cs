3206bd6f96d668b15ee2100a14d69b0f6ebacda5
tarnaldo88 <tarnaldo88@gmail.com>
2025-12-15 17:32:49 -0800

    Registered Infrastructure in API

--- Recuria/Recuria.Api/Program.cs
+++ Recuria/Recuria.Api/Program.cs
@@ -1,8 +1,23 @@
+using Recuria.Application;
+using Recuria.Infrastructure.Persistence;
+using Recuria.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Recuria.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<RecuriaDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IOrganizationRepository, OrganizationRepository>();
+builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+builder.Services.AddScoped<IInvoiceRepository, InvoiceRepository>();
+
 
 var app = builder.Build();
 
