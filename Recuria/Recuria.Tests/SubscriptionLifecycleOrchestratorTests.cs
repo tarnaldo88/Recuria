2beea01cf159174b7b9efb09d6670294c29655ac
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-08 10:40:10 -0800

    installed Moq, and starting to make unit tests for sub lifecycle Orch

---
+++ Recuria/Recuria.Tests/SubscriptionLifecycleOrchestratorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Recuria.Application;
+using Recuria.Domain;
+using Xunit;
+
+namespace Recuria.Tests
+{
+    public class SubscriptionLifecycleOrchestratorTests
+    {
+        private readonly Mock<IBillingService> _billingService;
+        private readonly SubscriptionLifecycleOrchestrator _orchestrator;
+        private readonly Organization _org;
+
+        public SubscriptionLifecycleOrchestratorTests()
+        {
+            _billingService = new Mock<IBillingService>();
+            _orchestrator = new SubscriptionLifecycleOrchestrator(_billingService.Object);
+            _org = new Organization("Test Org");
+        }
+    }
+}
