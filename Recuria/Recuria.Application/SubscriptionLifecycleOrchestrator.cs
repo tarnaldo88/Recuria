e2ed46d0f5f94c4e4725ef8c6bad2472be02ea52
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-08 10:01:16 -0800

    Writing methods for handle trial and handle active

--- Recuria/Recuria.Application/SubscriptionLifecycleOrchestrator.cs
+++ Recuria/Recuria.Application/SubscriptionLifecycleOrchestrator.cs
@@ -23,18 +23,47 @@ namespace Recuria.Application
                 case SubscriptionStatus.Trial:
                     HandleTrial(subscription, now);
                     break;
+
+                case SubscriptionStatus.Active:
+                    HandleActive(subscription, now);
+                    break;
+
+                case SubscriptionStatus.PastDue: 
+                    HandlePastDue(subscription, now);
+                    break;
+
+                case SubscriptionStatus.Canceled:
+                case SubscriptionStatus.Expired:
+                    break;
+
+                default:
+                    throw new InvalidOperationException($"Unhandled subscription status: {subscription.Status}");
             }
 
         }
 
         private void HandleTrial(Subscription subscription, DateTime now)
         {
-
+            if( now >= subscription.PeriodEnd)
+            {
+                subscription.Expire(now);
+            }
         }
 
         private void HandleActive(Subscription subscription, DateTime now)
         {
+            if (now < subscription.PeriodEnd)
+                return;
 
+            try
+            {
+                _billingService.RunBillingCycle(subscription,now);
+                subscription.AdvancePeriod(now);
+            }
+            catch (Exception ex)
+            {
+                subscription.MarkPastDue();
+            }
         }
 
         private void HandlePastDue(Subscription subscription, DateTime now)
