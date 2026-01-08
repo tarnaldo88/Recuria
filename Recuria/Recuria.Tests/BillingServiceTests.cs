9e80f5b00ed9592a8c01279bb4c56bc3852a479e
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-07 17:06:58 -0800

    Created todo list in test file, private method to create active sub

--- Recuria/Recuria.Tests/BillingServiceTests.cs
+++ Recuria/Recuria.Tests/BillingServiceTests.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using FluentAssertions;
+//using Recuria.Services;
+using Recuria.Application;
+using Recuria.Domain;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using FluentAssertions;
-using Recuria.Domain;
-//using Recuria.Services;
-using Recuria.Application;
 
 namespace Recuria.Tests
 {
@@ -20,5 +21,33 @@ namespace Recuria.Tests
             _service = new SubscriptionService();
             _org = new Organization("Test Org");
         }
+        private Subscription CreateActiveSubscription(
+            PlanType plan = PlanType.Pro,
+            DateTime? periodStart = null,
+            DateTime? periodEnd = null)
+        {
+            var start = periodStart ?? DateTime.UtcNow.AddMonths(-1);
+            var end = periodEnd ?? DateTime.UtcNow;
+
+            var sub = new Subscription(_org, plan, SubscriptionStatus.Active,start, end);
+            _org.AssignSubscription(sub);
+
+            return sub;
+        }
+
+        /*TESTS TO WRITE NEXT */
+        //Billing creates invoice and advances period
+
+        //Billing throws before period end
+
+        //PastDue subscription cancels after grace period
+
+        //PastDue subscription remains after grace not exceeded
+
+        //Billing refuses inactive subscriptions
+
+        //[Fact]
+        //public void 
+
     }
 }
