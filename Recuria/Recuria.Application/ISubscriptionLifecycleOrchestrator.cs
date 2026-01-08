1df22c7051422137797cf11b9f20aedca9c27ac3
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-08 09:54:02 -0800

    made process method for interface, sub lifecycle now inherits from interface

--- Recuria/Recuria.Application/ISubscriptionLifecycleOrchestrator.cs
+++ Recuria/Recuria.Application/ISubscriptionLifecycleOrchestrator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recuria.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Recuria.Application
 {
     internal interface ISubscriptionLifecycleOrchestrator
     {
-
+        void Process(Subscription subscription, DateTime now);
     }
 }
--- Recuria/Recuria.Application/SubscriptionLifecycleOrchestrator.cs
+++ Recuria/Recuria.Application/SubscriptionLifecycleOrchestrator.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Recuria.Application
 {
-    internal class SubscriptionLifecycleOrchestrator
+    public class SubscriptionLifecycleOrchestrator : ISubscriptionLifecycleOrchestrator
     {
     }
 }
