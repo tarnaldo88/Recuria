28b516de6a680fe3266f45a54918c9bf8699a8a8
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-07 16:38:58 -0800

    Created Billing service tests

---
+++ Recuria/Recuria.Tests/BillingServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Recuria.Domain;
+//using Recuria.Services;
+using Recuria.Application;
+
+namespace Recuria.Tests
+{
+    internal class BillingServiceTests
+    {
+        private readonly SubscriptionService _service;
+        private readonly Organization _org;
+
+        public BillingServiceTests()
+        {
+            _service = new SubscriptionService();
+            _org = new Organization("Test Org");
+        }
+    }
+}
