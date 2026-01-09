421c8fd648cdacf902cc63d077ea9b5c9536d928
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 15:26:23 -0800

    Added SubscriptionPastDue

---
+++ Recuria/Recuria.Domain/Events/SubscriptionPastDue.cs
@@ -0,0 +1,14 @@
+﻿using Recuria.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Events
+{
+    public sealed record SubscriptionPastDue : IDomainEvent
+    {
+        public DateTime OccuredOn { get; } = DateTime.UtcNow;
+    }
+}
