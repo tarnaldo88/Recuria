67697cfcddfb109dc6b1ea6a3cd067f0e526c431
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 08:21:31 -0800

    Private members added to class

---
+++ Recuria/Recuria.Domain/BillingAttempt.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain
+{
+    public class BillingAttempt
+    {
+        public Guid Id { get; }
+        public Guid SubscriptionId { get; }
+        public DateTime AttemptedAt { get; }
+        public bool Succeeded { get; }
+        public string? FailureReason { get; }
+    }
+}
