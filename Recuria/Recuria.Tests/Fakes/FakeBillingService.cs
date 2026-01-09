d173e594c69897eeedb7b470a6b21e85088c086f
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 12:29:34 -0800

    Making fake billing service for testing.

---
+++ Recuria/Recuria.Tests/Fakes/FakeBillingService.cs
@@ -0,0 +1,31 @@
+﻿using Recuria.Application;
+using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests.Fakes
+{
+    public class FakeBillingService : IBillingService
+    {
+        private readonly Queue<Exception?> _results = new();
+
+        public void QueueFailure(Exception exception) => _results.Enqueue(exception);
+        public void QueueSuccess() => _results.Enqueue(null);
+
+        public Invoice RunBillingCycle(Subscription subscription, DateTime now)
+        {
+            if (_results.Count == 0)
+                throw new InvalidOperationException("No billing result queued.");
+
+            var result = _results.Dequeue();
+
+            if (result != null)
+                throw result;
+
+            return new Invoice(subscription.Id, 29m);
+        }
+    }
+}
