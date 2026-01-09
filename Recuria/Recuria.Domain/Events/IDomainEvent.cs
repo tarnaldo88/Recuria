924d9da0c48a09c9f7567c155e2de2460a8d9716
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 15:09:09 -0800

    Added Domain Event interface

---
+++ Recuria/Recuria.Domain/Events/IDomainEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Events
+{
+    public interface IDomainEvent
+    {
+        DateTime OccuredOn { get; }
+    }
+}
