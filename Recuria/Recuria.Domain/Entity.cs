292c9782d82cf3686bdcb08b6ce395ed2e27e59c
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 15:11:26 -0800

    Added Entity class

---
+++ Recuria/Recuria.Domain/Entity.cs
@@ -0,0 +1,23 @@
+﻿using Recuria.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain
+{
+    public abstract class Entity
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents;
+
+        protected void RaiseDomainEvent(IDomainEvent @event)
+        {
+            _domainEvents.Add(@event);
+        }
+
+        public void ClearDomainEvents() => _domainEvents.Clear();
+    }
+}
