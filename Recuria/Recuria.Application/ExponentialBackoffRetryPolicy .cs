dbf6378cd10c9cc354f32c70cfd62d3973ee7347
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-08 22:45:30 -0800

    Writing class member and method ShouldRetry

---
+++ Recuria/Recuria.Application/ExponentialBackoffRetryPolicy .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public class ExponentialBackoffRetryPolicy : IBillingRetryPolicy
+    {
+        private const int MaxAttempts = 3;
+
+        public bool ShouldRetry(int attempt, Exception exception)
+        {
+            return attempt < MaxAttempts &&
+                   exception is InvalidOperationException;
+        }
+    }
+}
