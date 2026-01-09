db5327ec0d3c3fc76ab0ff67c3aecbc3bbc7fa05
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-09 12:31:45 -0800

    made fixed retry policy and its constructor

---
+++ Recuria/Recuria.Tests/Fakes/FixedRetryPolicy.cs
@@ -0,0 +1,19 @@
+﻿using Recuria.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests.Fakes
+{
+    public class FixedRetryPolicy : IBillingRetryPolicy
+    {
+        private readonly int _maxRetries;
+
+        public FixedRetryPolicy(int maxRetries)
+        {
+            _maxRetries = maxRetries;
+        }
+    }
+}
