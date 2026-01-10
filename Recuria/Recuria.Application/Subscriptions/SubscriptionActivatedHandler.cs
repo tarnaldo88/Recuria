7d38e685a73f7b4402831645a46b788231c24402
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-10 09:38:55 -0800

    Added sub activated handler

---
+++ Recuria/Recuria.Application/Subscriptions/SubscriptionActivatedHandler.cs
@@ -0,0 +1,26 @@
+﻿using Recuria.Domain.Abstractions;
+using Recuria.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Subscriptions
+{
+    public sealed class SubscriptionActivatedHandler : IDomainEventHandler<SubscriptionActivatedHandler>
+    {
+        public Task HandleAsync(SubscriptionActivatedHandler domainEvent, CancellationToken cancellationToken)
+        {
+            // Placeholder for real behavior:
+            // - Send welcome email
+            // - Emit integration event
+            // - Provision tenant resources
+
+            Console.WriteLine(
+                $"Subscription {domainEvent.SubscriptionId} activated for Org {domainEvent.OrganizationId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
