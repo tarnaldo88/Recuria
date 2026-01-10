6230e2f2a134f49261c25a6e07ed56bdc3268a57
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-10 07:43:38 -0800

    Created IDomainEventHandler

---
+++ Recuria/Recuria.Domain/Abstractions/IDomainEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Abstractions
+{
+    public interface IDomainEventHandler<in TEvent> where TEvent : IDomainEvent
+    {
+        Task HandleAsync(TEvent domainEvent, CancellationToken cancellationToken);
+    }
+}
