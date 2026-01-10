ed56638bb2eeee6907f94696e4a87983437513a1
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-10 07:40:30 -0800

    Created DomainEventDispatcher

---
+++ Recuria/Recuria.Infrastructure/Persistence/DomainEventDispatcher .cs
@@ -0,0 +1,17 @@
+﻿using Recuria.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Persistence
+{
+    public class DomainEventDispatcher : IDomainEventDispatcher
+    {
+        public Task DispatchAsync(IDomainEvent domainEvent, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
