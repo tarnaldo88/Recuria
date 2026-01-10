08eb23a10c4c45f954a6054b646d8befbc7521a4
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-10 07:45:25 -0800

    Created Sub expired handler

---
+++ Recuria/Recuria.Application/Billing/SubscriptionExpiredHandler.cs
@@ -0,0 +1,21 @@
+﻿using Recuria.Domain.Abstractions;
+using Recuria.Domain.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Billing
+{
+    public class SubscriptionExpiredHandler : IDomainEventHandler<SubscriptionExpired>
+    {
+        public Task HandleAsync(SubscriptionExpired domainEvent, CancellationToken ct)
+        {
+            // Send email
+            // Emit integration event
+            // Audit log
+            return Task.CompletedTask;
+        }
+    }
+}
