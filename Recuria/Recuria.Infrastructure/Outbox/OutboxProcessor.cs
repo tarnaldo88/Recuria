f195b3259bcd79c5ed12448785b691d5e840eab8
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-11 11:10:57 -0800

    Created outbox processor

---
+++ Recuria/Recuria.Infrastructure/Outbox/OutboxProcessor.cs
@@ -0,0 +1,25 @@
+﻿using Recuria.Domain.Abstractions;
+using Recuria.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Outbox
+{
+    public sealed class OutboxProcessor
+    {
+        private readonly RecuriaDbContext _db;
+        private readonly IDomainEventDispatcher _dispatcher;
+
+        public OutboxProcessor(
+            RecuriaDbContext db,
+            IDomainEventDispatcher dispatcher)
+        {
+            _db = db;
+            _dispatcher = dispatcher;
+        }
+
+    }
+}
