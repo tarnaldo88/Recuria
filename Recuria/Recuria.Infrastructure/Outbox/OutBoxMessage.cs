3214a2c6c157f1932e8385d77585b267435f42c5
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-11 10:33:17 -0800

    Created OutBox message class

---
+++ Recuria/Recuria.Infrastructure/Outbox/OutBoxMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Outbox
+{
+    public sealed class OutBoxMessage
+    {
+        public Guid Id { get; init; }
+        public DateTime OccurredOnUtc { get; init; }
+        public string Type { get; init; } = null!;
+        public string Content { get; init; } = null!;
+
+        public DateTime? ProcessedOnUtc { get; set; }
+        public string? Error { get; set; }
+    }
+}
