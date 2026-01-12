7ba422a7148e8280a3a0fcb14bb051e6598e5896
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 12:51:31 -0800

    Created Processed Event class

---
+++ Recuria/Recuria.Infrastructure/Idempotency/ProcessedEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Idempotency
+{
+    public sealed class ProcessedEvent
+    {
+        public Guid EventId { get; private set; }
+        public DateTime ProcessedOnUtc { get; private set; }
+
+        private ProcessedEvent() { } //EF Core
+
+        public ProcessedEvent(Guid eventId)
+        {
+            EventId = eventId;
+            ProcessedOnUtc = DateTime.UtcNow;
+        }
+    }
+}
