b948170c7eba7737209faaf41fe9d86b8bec7f74
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 12:01:17 -0800

    Added Execute Async

--- Recuria/Recuria.Infrastructure/Outbox/OutboxProcessorHostedService.cs
+++ Recuria/Recuria.Infrastructure/Outbox/OutboxProcessorHostedService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Recuria.Infrastructure.Outbox
 {
-    public sealed class OutboxProcessorHostedService
+    public sealed class OutboxProcessorHostedService : BackgroundService
     {
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly ILogger<OutboxProcessorHostedService> _logger;
@@ -19,6 +20,26 @@ namespace Recuria.Infrastructure.Outbox
         {
             _scopeFactory = scopeFactory;
             _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    using var scope = _scopeFactory.CreateScope();
+                    var processor = scope.ServiceProvider.GetRequiredService<OutboxProcessor>();
+
+                    await processor.ProcessAsync(stoppingToken);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Outbox processing failed");
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(5), stoppingToken);
+            }
         }
     }
 }
