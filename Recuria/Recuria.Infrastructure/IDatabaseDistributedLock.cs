611966a47e42d39121dae0841937dad14b72a3a5
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 14:52:09 -0800

    added interface

---
+++ Recuria/Recuria.Infrastructure/IDatabaseDistributedLock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure
+{
+    public interface IDatabaseDistributedLock
+    {
+        Task<bool> TryAcquireAsync(string lockName, CancellationToken ct);
+        Task ReleaseAsync(string lockName, CancellationToken ct);
+    }
+}
