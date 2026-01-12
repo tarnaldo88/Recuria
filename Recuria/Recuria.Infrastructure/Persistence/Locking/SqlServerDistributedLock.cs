44ccb347228efa73de67a6cfacbcc184b1b87ca9
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 14:53:41 -0800

    Added sql server distrub lock

---
+++ Recuria/Recuria.Infrastructure/Persistence/Locking/SqlServerDistributedLock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Persistence.Locking
+{
+    public sealed class SqlServerDistributedLock : IDatabaseDistributedLock
+    {
+        private readonly RecuriaDbContext _db;
+
+        public SqlServerDistributedLock(RecuriaDbContext db)
+        {
+            _db = db;
+        }
+    }
+}
