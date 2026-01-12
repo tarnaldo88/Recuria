030cb0c896d042214ce379a6d0944128240aa721
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 12:41:19 -0800

    Created EF Processed Event store

---
+++ Recuria/Recuria.Infrastructure/Idempotency/EfProcessedEventStore.cs
@@ -0,0 +1,30 @@
+﻿using Recuria.Application.Interface.Idempotency;
+using Recuria.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Idempotency
+{
+    internal sealed class EfProcessedEventStore : IProcessedEventStore
+    {
+        private readonly RecuriaDbContext _db;
+
+        public EfProcessedEventStore(RecuriaDbContext db)
+        {
+            _db = db;
+        }
+
+        public Task<bool> ExistsAsync(Guid eventId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task MarkProcessedAsync(Guid eventId, CancellationToken ct)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
