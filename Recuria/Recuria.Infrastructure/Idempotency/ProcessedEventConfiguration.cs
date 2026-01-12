f4a79595aa9074cdf35deedabd4775f23d6b564c
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 12:57:15 -0800

    Fixed typo

--- Recuria/Recuria.Infrastructure/Idempotency/ProcessedEventConfiguration.cs
+++ Recuria/Recuria.Infrastructure/Idempotency/ProcessedEventConfiguration.cs
@@ -10,17 +10,12 @@ namespace Recuria.Infrastructure.Idempotency
 {
     internal sealed class ProcessedEventConfiguration : IEntityTypeConfiguration<ProcessedEvent>
     {
-        public ProcessedEventConfiguration( EntityTypeBuilder<ProcessedEvent> builder)
+        public void Configure( EntityTypeBuilder<ProcessedEvent> builder)
         {
             builder.ToTable("ProcessedEvents");
             builder.HasKey(x => x.EventId);
 
             builder.Property(x => x.ProcessedOnUtc).IsRequired();
-        }
-
-        public void Configure(EntityTypeBuilder<ProcessedEvent> builder)
-        {
-            throw new NotImplementedException();
         }
     }
 }
