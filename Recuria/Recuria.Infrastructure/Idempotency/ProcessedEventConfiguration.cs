f01e5721d3338792efed066069beef79ab27f8e0
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 12:56:12 -0800

    Created Processed Event config

---
+++ Recuria/Recuria.Infrastructure/Idempotency/ProcessedEventConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Recuria.Infrastructure.Idempotency
+{
+    internal sealed class ProcessedEventConfiguration : IEntityTypeConfiguration<ProcessedEvent>
+    {
+        public ProcessedEventConfiguration( EntityTypeBuilder<ProcessedEvent> builder)
+        {
+            builder.ToTable("ProcessedEvents");
+            builder.HasKey(x => x.EventId);
+
+            builder.Property(x => x.ProcessedOnUtc).IsRequired();
+        }
+
+        public void Configure(EntityTypeBuilder<ProcessedEvent> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
