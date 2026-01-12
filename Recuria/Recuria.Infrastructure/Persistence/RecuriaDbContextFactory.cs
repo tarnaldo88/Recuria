69dd7ace4473d0d873d2580122d2b6731ef16da2
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-12 14:35:31 -0800

    Added recuria db factory

---
+++ Recuria/Recuria.Infrastructure/Persistence/RecuriaDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Persistence
+{
+    public sealed class RecuriaDbContextFactory : IDesignTimeDbContextFactory<RecuriaDbContext>
+    {
+        public RecuriaDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<RecuriaDbContext>()
+                .UseSqlServer(
+                    "Server=(localdb)\\mssqllocaldb;Database=Recuria;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+
+            return new RecuriaDbContext(options);
+        }
+    }
+}
