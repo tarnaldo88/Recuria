6111341da8828a5a320b529e49c405f13612eb2b
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-13 10:27:02 -0800

    SubscriptionActivatedIntegrationEvent created

---
+++ Recuria/Recuria.Domain/Events/Subscription/SubscriptionActivatedIntegrationEvent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Events.Subscription
+{
+    public sealed class SubscriptionActivatedIntegrationEvent
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public DateTime OccurredOnUtc { get; } = DateTime.UtcNow;
+
+        public Guid SubscriptionId { get; }
+
+        public SubscriptionActivatedIntegrationEvent(Guid subscriptionId)
+        {
+            SubscriptionId = subscriptionId;
+        }
+    }
+}
