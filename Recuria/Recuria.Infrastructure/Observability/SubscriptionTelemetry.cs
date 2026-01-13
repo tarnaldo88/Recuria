247374a300ee76525205783c7bc8b82502ec65e4
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-13 15:04:29 -0800

    created sub telemetry

---
+++ Recuria/Recuria.Infrastructure/Observability/SubscriptionTelemetry.cs
@@ -0,0 +1,32 @@
+﻿using Recuria.Application.Observability;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Observability
+{
+    public sealed class SubscriptionTelemetry : ISubscriptionTelemetry
+    {
+        public IDisposable BeginProcessing(Guid subscriptionId, string status)
+        {
+            var activity = Telemetry.ActivitySource.StartActivity(
+                "SubscriptionLifecycle.Process");
+
+            activity?.SetTag("subscription.id", subscriptionId);
+            activity?.SetTag("subscription.status", status);
+
+            return activity ?? NullScope.Instance;
+        }
+
+        public void BillingAttempted()
+            => Telemetry.BillingAttempts.Add(1);
+
+        public void BillingFailed()
+            => Telemetry.BillingFailures.Add(1);
+
+        public void SubscriptionActivated()
+            => Telemetry.SubscriptionsActivated.Add(1);
+    }
+}
