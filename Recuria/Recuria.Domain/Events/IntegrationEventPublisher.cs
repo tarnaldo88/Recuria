d289099b2528aea69182463dd92cd41d9e558c17
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-13 10:30:07 -0800

    IntegrationEventPublisher created

---
+++ Recuria/Recuria.Domain/Events/IntegrationEventPublisher.cs
@@ -0,0 +1,17 @@
+﻿using Recuria.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Events
+{
+    public sealed class IntegrationEventPublisher : IIntegrationEventPublisher
+    {
+        public Task PublishAsync(IIntegrationEvent @event, CancellationToken ct)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
