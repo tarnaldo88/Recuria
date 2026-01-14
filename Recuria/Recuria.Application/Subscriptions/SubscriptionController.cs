1c424315f46a3b35ad9f89898ac84d5ebdb08ec1
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:08:25 -0800

    Created sub controller

---
+++ Recuria/Recuria.Application/Subscriptions/SubscriptionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Subscriptions
+{
+    public sealed class SubscriptionController : ControllerBase
+    {
+        private readonly ISubscriptionQueries _queries;
+
+        public SubscriptionController(ISubscriptionQueries queries)
+        {
+            _queries = queries;
+        }
+
+        [HttpGet("current")]
+        public async Task<ActionResult<SubscriptionDto>> GetCurrent()
+        {
+            var orgId = /* resolve from auth later */;
+            var subscription = await _queries.GetCurrentAsync(orgId);
+
+            if (subscription is null)
+                return NotFound();
+
+            return Ok(subscription);
+        }
+    }
+}
