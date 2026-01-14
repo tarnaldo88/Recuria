e7429e3d880ac97c86a9657d6050b86108139724
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:53:27 -0800

    Created sub queries interface

---
+++ Recuria/Recuria.Infrastructure/Persistence/Queries/ISubscriptionQueries.cs
@@ -0,0 +1,16 @@
+﻿using Recuria.Application.Contracts.Subscription;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Infrastructure.Persistence.Queries
+{
+    public interface ISubscriptionQueries
+    {        
+        Task<SubscriptionDetailsDto?> GetCurrentAsync(
+            Guid organizationId,
+            CancellationToken ct);
+    }
+}
