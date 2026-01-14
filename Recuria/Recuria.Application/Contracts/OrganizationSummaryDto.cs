a8a60d510782e08dd7815fab2d0e4ae281562b29
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:18:48 -0800

    created dto

---
+++ Recuria/Recuria.Application/Contracts/OrganizationSummaryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts
+{
+    public sealed record OrganizationSummaryDto(
+        Guid Id,
+        string Name,
+        int UserCount,
+        string SubscriptionStatus
+    );
+}
