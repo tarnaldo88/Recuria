04e3a853c3d7474daff52a3a18224a214be885d1
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:23:47 -0800

    created dto

---
+++ Recuria/Recuria.Application/Contracts/InvoiceDetailsDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts
+{
+    public sealed record InvoiceDetailsDto(
+        Guid Id,
+        string InvoiceNumber,
+        DateTime IssuedOnUtc,
+        DateTime? PaidOnUtc,
+        MoneyDto Subtotal,
+        MoneyDto Tax,
+        MoneyDto Total,
+        string Status
+    );
+}
