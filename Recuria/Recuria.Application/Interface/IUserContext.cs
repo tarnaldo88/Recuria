da803805d4baaedda343a16e8b39eb040a4a567d
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:12:09 -0800

    created interface

---
+++ Recuria/Recuria.Application/Interface/IUserContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface
+{
+    public interface IUserContext
+    {
+        Guid UserId { get; }
+        Guid OrganizationId { get; }
+    }
+}
