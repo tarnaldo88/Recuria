ee54f7022f74b7b7c29afd707321d516030d71e6
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:10:24 -0800

    Created Sub Dto

---
+++ Recuria/Recuria.Application/Subscriptions/SubscriptionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Subscriptions
+{
+    public sealed record SubscriptionDto(Guid Id,
+    string Plan,
+    string Status,
+    DateTime PeriodStart,
+    DateTime PeriodEnd);
+}
