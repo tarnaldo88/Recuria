f29db9d4aed4f5252c5e844069a82f67fa7e6706
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:17:58 -0800

    created dto

---
+++ Recuria/Recuria.Application/Contracts/CurrentUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts
+{
+    public sealed record CurrentUserDto(
+        Guid Id,
+        string Email,
+        string Role,
+        Guid OrganizationId
+    );
+}
