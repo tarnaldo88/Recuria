863547113721115e494b6ed293e3d0851d7f9151
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 11:34:49 -0800

    added scrutor to proj, and created infrastructure service collection extensions

---
+++ Recuria/Recuria.Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Recuria.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scrutor;
+using Recuria.Infrastructure.Persistence.Queries;
+using Recuria.Application.Interface;
+
+namespace Recuria.Infrastructure
+{
+    public static class InfrastructureServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<RecuriaDbContext>(...);
+
+            services.Scan(scan => scan
+                .FromAssemblyOf<SubscriptionQueries>()
+                .AddClasses(c => c.AssignableTo(typeof(ISubscriptionQueries)))
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+            return services;
+        }
+    }
+}
