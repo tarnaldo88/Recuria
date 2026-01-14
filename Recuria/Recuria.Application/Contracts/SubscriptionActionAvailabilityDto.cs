99f3391305859746bbe944c9035ff0e26b4d58c3
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 10:20:42 -0800

    Created Dto

---
+++ Recuria/Recuria.Application/Contracts/SubscriptionActionAvailabilityDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts
+{
+    public sealed record SubscriptionActionAvailabilityDto(
+        bool CanActivate,
+        bool CanCancel,
+        bool CanUpgrade
+    );
+
+}
