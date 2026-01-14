8d18fa27b275f950c90d72fdfd32801ef93e1a08
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-14 12:19:44 -0800

    Added invoice controller

---
+++ Recuria/Recuria.Api/invoices/InvoiceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Recuria.Application.Contracts.Invoice;
+using Recuria.Infrastructure.Persistence.Queries.Interface;
+
+namespace Recuria.Api.invoices
+{
+    public class InvoiceController : Controller
+    {
+        private readonly IInvoiceQueries _invoiceQueries;
+
+        public InvoiceController(IInvoiceQueries invoiceQueries)
+        {
+            _invoiceQueries = invoiceQueries;
+        }
+
+        [HttpGet("organization/{organizationId:guid}")]
+        public async Task<ActionResult<IReadOnlyList<InvoiceListItemDto>>> GetForOrganization(Guid organizationId, CancellationToken ct)
+        {
+            var invoices = await _invoiceQueries.GetForOrganizationAsync(organizationId, ct);
+            return Ok(invoices);
+        }
+
+        [HttpGet("{invoiceId:guid}")]
+        public async Task<ActionResult<InvoiceDetailsDto>> GetDetails(Guid invoiceId, CancellationToken ct)
+        {
+            var invoice = await _invoiceQueries.GetDetailsAsync(invoiceId, ct);
+            if (invoice == null) return NotFound();
+            return Ok(invoice);
+        }
+    }
+}
