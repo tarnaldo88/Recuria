bf922cfa2e634944d57c3cf86a8b4567763a5736
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-15 13:07:49 -0800

    created create org request file

---
+++ Recuria/Recuria.Application/CreateOrganizationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public class CreateOrganizationRequest
+    {
+        public string Name { get; init; } = string.Empty;
+
+        public string OwnerEmail { get; init; } = string.Empty;
+
+        public string OwnerFirstName { get; init; } = string.Empty;
+
+        public string OwnerLastName { get; init; } = string.Empty;
+    }
+}
