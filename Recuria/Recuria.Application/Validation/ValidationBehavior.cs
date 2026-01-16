531f663faeb8fcca06db7f03c14113abc89cda21
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 11:51:47 -0800

    created validation behav

---
+++ Recuria/Recuria.Application/Validation/ValidationBehavior.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Validation
+{
+    public class ValidationBehavior
+    {
+        private readonly IEnumerable<IValidator> _validators;
+
+        public ValidationBehavior(IEnumerable<IValidator> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task ValidateAsync<T>(T instance)
+        {
+            var context = new ValidationContext<T>(instance);
+
+            var failures = _validators
+                .OfType<IValidator<T>>()
+                .Select(v => v.Validate(context))
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Any())
+                throw new ValidationException(failures);
+        }
+    }
+}
