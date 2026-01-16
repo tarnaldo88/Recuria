3ad16b7e191b045b83c27b53d10c366e10821529
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 10:44:52 -0800

    refactored and implemented methods

--- Recuria/Recuria.Infrastructure/Repositories/InvoiceRepository .cs
+++ Recuria/Recuria.Infrastructure/Repositories/InvoiceRepository .cs
@@ -13,22 +13,28 @@ namespace Recuria.Infrastructure.Repositories
 {
     public class InvoiceRepository : IInvoiceRepository
     {
-        private readonly RecuriaDbContext _context;
+        private readonly RecuriaDbContext _db;
 
-        public InvoiceRepository(RecuriaDbContext context)
+        public InvoiceRepository(RecuriaDbContext db)
         {
-            _context = context;
+            _db = db;
         }
 
-        public async Task AddAsync(Invoice invoice)
+        public Task<Invoice?> GetByIdAsync(Guid id, CancellationToken ct) =>
+            _db.Invoices.FirstOrDefaultAsync(i => i.Id == id, ct);
+
+        public async Task AddAsync(Invoice invoice, CancellationToken ct)
         {
-            _context.Invoices.Add(invoice);
-            await _context.SaveChangesAsync();
+            _db.Invoices.Add(invoice);
+            await _db.SaveChangesAsync(ct);
         }
+
+        public Task SaveChangesAsync(CancellationToken ct) =>
+            _db.SaveChangesAsync(ct);
 
         public async Task<IReadOnlyList<Invoice>> GetBySubscriptionIdAsync(Guid subscriptionId)
         {
-            return await _context.Invoices
+            return await _db.Invoices
                 .Where(i => i.SubscriptionId == subscriptionId)
                 .ToListAsync();
         }
