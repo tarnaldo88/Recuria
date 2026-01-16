09a0db001a0e59a1332a5ecc2066d97625bb2cf4
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 11:22:56 -0800

    created invoice requests and added to validator

---
+++ Recuria/Recuria.Application/Contracts/Invoice/Validators/CreateInvoiceRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using Recuria.Application.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts.Invoice.Validators
+{
+    public class CreateInvoiceRequestValidator : AbstractValidator<CreateInvoiceRequest>
+    {
+        public CreateInvoiceRequestValidator()
+        {
+            RuleFor(x => x.OrganizationId)
+                .NotEmpty();
+
+            RuleFor(x => x.Amount)
+                .GreaterThan(0)
+                .LessThan(100_000);
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MaximumLength(200);
+        }
+    }
+    {
+    }
+}
---
+++ Recuria/Recuria.Application/Requests/CreateInvoiceRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Requests
+{
+    public class CreateInvoiceRequest
+    {
+        public Guid OrganizationId { get; init; }
+        public decimal Amount { get; init; }
+        public string Description { get; init; } = string.Empty;
+    }
+}
