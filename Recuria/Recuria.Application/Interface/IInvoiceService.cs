320a9e707087cb980d77b1097cac53d0d69e998a
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 10:40:30 -0800

    created interface

---
+++ Recuria/Recuria.Application/Interface/IInvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using Recuria.Application.Contracts.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface
+{
+    public interface IInvoiceService
+    {
+        Task<Guid> CreateInvoiceAsync(
+        Guid subscriptionId,
+        MoneyDto amount,
+        CancellationToken ct);
+
+        Task MarkPaidAsync(Guid invoiceId, CancellationToken ct);
+    }
+}
