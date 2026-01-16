a860b657483f75d4367ecf7f5fa741e24a112fdf
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-15 17:14:59 -0800

    Created

---
+++ Recuria/Recuria.Application/Interface/Abstractions/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface.Abstractions
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(Guid id, CancellationToken ct);
+    }
+}
