bb84f3aaf28d11dc9dd87780f22768c742378fdc
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-15 17:20:15 -0800

    Created interface

---
+++ Recuria/Recuria.Application/Interface/IOrganizationQueries.cs
@@ -0,0 +1,28 @@
+﻿using Recuria.Application.Contracts.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface
+{
+    public interface IOrganizationQueries
+    {
+        Task<bool> ExistsAsync(
+            Guid id,
+            CancellationToken cancellationToken);
+
+        Task<OrganizationDto?> GetByIdAsync(
+            Guid id,
+            CancellationToken cancellationToken);
+
+        Task<OrganizationDto?> GetByUserIdAsync(
+            Guid userId,
+            CancellationToken cancellationToken);
+
+        Task<OrganizationSummaryDto?> GetAsync(
+            Guid organizationId,
+            CancellationToken cancellationToken);
+    }
+}
