9a2d12e92d033bcb6713bb9a7330f87747c5da50
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 10:43:25 -0800

    Some fixes on member names and method calls

---
+++ Recuria/Recuria.Application/InvoiceService.cs
@@ -0,0 +1,61 @@
+﻿using Recuria.Application.Contracts.Common;
+using Recuria.Application.Interface;
+using Recuria.Application.Interface.Abstractions;
+using Recuria.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application
+{
+    public class InvoiceService : IInvoiceService
+    {
+        private readonly IInvoiceRepository _invoices;
+        private readonly ISubscriptionRepository _subscriptions;
+
+        public InvoiceService(
+            IInvoiceRepository invoices,
+            ISubscriptionRepository subscriptions)
+        {
+            _invoices = invoices;
+            _subscriptions = subscriptions;
+        }
+
+        public async Task<Guid> CreateInvoiceAsync(
+            Guid subscriptionId,
+            MoneyDto amount,
+            CancellationToken ct)
+        {
+            var subscription =
+                await _subscriptions.GetByIdAsync(subscriptionId, ct);
+
+            if (subscription == null)
+                throw new InvalidOperationException("Subscription not found");
+
+            var invoice = new Invoice(
+                subscriptionId,
+                amount.Amount);
+
+            await _invoices.AddAsync(invoice, ct);
+
+            return invoice.Id;
+        }
+
+        public async Task MarkPaidAsync(
+            Guid invoiceId,
+            CancellationToken ct)
+        {
+            var invoice = await _invoices.GetByIdAsync(invoiceId, ct);
+
+            if (invoice == null)
+                throw new InvalidOperationException("Invoice not found");
+
+            invoice.MarkAsPaid();
+
+            await _invoices.SaveChangesAsync(ct);
+        }
+    }
+
+}
