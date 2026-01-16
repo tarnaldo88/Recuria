0d6c0678103e3930ca03d4c785d8a120ff4498fb
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 11:20:33 -0800

    created shared rules

---
+++ Recuria/Recuria.Application/Validation/Common/SharedRules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Recuria.Application.Validation.Common
+{
+    public static class SharedRules
+    {
+        public static IRuleBuilderOptions<T, string> ValidName<T>(
+            this IRuleBuilder<T, string> rule)
+        {
+            return rule
+                .NotEmpty()
+                .MinimumLength(2)
+                .MaximumLength(80);
+        }
+
+        public static IRuleBuilderOptions<T, string> ValidEmail<T>(
+            this IRuleBuilder<T, string> rule)
+        {
+            return rule
+                .NotEmpty()
+                .EmailAddress()
+                .MaximumLength(120);
+        }
+    }
+}
