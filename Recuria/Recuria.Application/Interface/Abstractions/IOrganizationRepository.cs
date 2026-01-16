9ef3078bad9116c5b67ae7516a21f2d997ad69e5
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-15 17:03:33 -0800

    Created interface

---
+++ Recuria/Recuria.Application/Interface/Abstractions/IOrganizationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Recuria.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface.Abstractions
+{
+    public interface IOrganizationRepository
+    {
+        Task<Organization?> GetByIdAsync(Guid id, CancellationToken ct);
+    }
+}
