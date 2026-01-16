30744b3cfe20905ba0033b264dae094608bf2c92
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 12:03:56 -0800

    created

---
+++ Recuria/Recuria.Application/Interface/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync(CancellationToken ct = default);
+    }
+}
