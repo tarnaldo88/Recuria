aff363cad302ed3b4500085c992c56e917b0e7c5
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 10:59:10 -0800

    created validator

---
+++ Recuria/Recuria.Application/Contracts/Subscription/Validators/UpgradeSubscriptionRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Recuria.Application.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts.Subscription.Validators
+{
+    public class UpgradeSubscriptionRequestValidator : AbstractValidator<UpgradeSubscriptionRequest>
+    {
+        public UpgradeSubscriptionRequestValidator()
+        {
+            RuleFor(x => x.NewPlan)
+                .IsInEnum();
+        }
+    }
+}
