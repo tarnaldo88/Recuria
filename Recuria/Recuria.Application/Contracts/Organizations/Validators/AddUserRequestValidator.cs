e303770fceeadbcfd14340776b80e2c56caff4dd
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 10:58:24 -0800

    Adding validators

---
+++ Recuria/Recuria.Application/Contracts/Organizations/Validators/AddUserRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Recuria.Application.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts.Organizations.Validators
+{
+    public class AddUserRequestValidator : AbstractValidator<AddUserRequest>
+    {
+        public AddUserRequestValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty();
+
+            RuleFor(x => x.Role)
+                .IsInEnum();
+        }
+    }
+}
---
+++ Recuria/Recuria.Application/Contracts/Organizations/Validators/CreateOrganizationRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Recuria.Application.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Recuria.Application.Contracts.Organizations.Validators
+{
+    public class CreateOrganizationRequestValidator : AbstractValidator<CreateOrganizationRequest>
+    {
+        public CreateOrganizationRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            RuleFor(x => x.OwnerId)
+                .NotEmpty();
+        }
+    }
+}
