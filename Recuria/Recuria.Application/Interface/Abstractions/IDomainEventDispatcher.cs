45173ddef588cd4c5b2bc3baf11f8044fb21dfd1
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 12:48:46 -0800

    created

---
+++ Recuria/Recuria.Application/Interface/Abstractions/IDomainEventDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using Recuria.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface.Abstractions
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(IEnumerable<IDomainEvent> events, CancellationToken ct);
+    }
+}
