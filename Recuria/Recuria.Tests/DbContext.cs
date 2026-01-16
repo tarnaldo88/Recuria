3dea17c8b5c9ecf27c96c611b6fe2d50a440d55d
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-16 13:41:35 -0800

    adding test files and package

---
+++ Recuria/Recuria.Tests/DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Recuria.Infrastructure.Persistence;
+
+namespace Recuria.Tests
+{
+    public static RecuriaDbContext BuildInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<RecuriaDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        return new RecuriaDbContext(options);
+    }
+}
---
+++ Recuria/Recuria.Tests/OrganizationSubscriptionInvoiceTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recuria.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests
+{
+    internal class OrganizationSubscriptionInvoiceTests
+    {
+        public static RecuriaDbContext BuildInMemoryDbContext()
+        {
+            var options = new DbContextOptionsBuilder<RecuriaDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new RecuriaDbContext(options);
+        }
+    }
+}
