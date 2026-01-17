4f7769e3803e06ff44e1b45fba762f34deb81944
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-17 10:47:36 -0800

    refactored org created domain event

--- Recuria/Recuria.Domain/Events/Organization/OrganizationCreatedDomainEvent.cs
+++ Recuria/Recuria.Domain/Events/Organization/OrganizationCreatedDomainEvent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Recuria.Domain.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace Recuria.Domain.Events.Organization
 {
-    public sealed record OrganizationCreatedDomainEvent(
-    Guid OrganizationId
-    ) : DomainEvent;
+    public sealed class OrganizationCreatedDomainEvent : IDomainEvent
+    {
+        public Guid OrganizationId { get; }
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+        public Guid EventId { get; } = Guid.NewGuid();
+
+        public OrganizationCreatedDomainEvent(Guid organizationId)
+        {
+            OrganizationId = organizationId;
+        }
+    }
 }
