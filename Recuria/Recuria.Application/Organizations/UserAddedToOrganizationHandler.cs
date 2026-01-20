8ae61551943bc94f3189d0e754b84ef3ea333c17
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-20 15:41:38 -0800

    created UserAddedToOrganizationHandler file and constructor.

---
+++ Recuria/Recuria.Application/Organizations/UserAddedToOrganizationHandler.cs
@@ -0,0 +1,25 @@
+﻿using Recuria.Application.Interface;
+using Recuria.Application.Interface.Abstractions;
+using Recuria.Domain.Events.Organization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Organizations
+{
+    public sealed class UserAddedToOrganizationHandler : IDomainEventHandler<UserAddedToOrganizationDomainEvent>
+    {
+        private readonly IUserRepository _users;
+        private readonly IUnitOfWork _uow;
+
+        public UserAddedToOrganizationHandler(
+            IUserRepository users,
+            IUnitOfWork uow)
+        {
+            _users = users;
+            _uow = uow;
+        }
+    }
+}
