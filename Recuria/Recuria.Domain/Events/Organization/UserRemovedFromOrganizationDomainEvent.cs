ed0fef9d556ad51f505d72d3110c9ae1111c52c6
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-20 15:19:10 -0800

    Created UserRemovedFromOrganizationDomainEvent class

---
+++ Recuria/Recuria.Domain/Events/Organization/UserRemovedFromOrganizationDomainEvent.cs
@@ -0,0 +1,25 @@
+﻿using Recuria.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Events.Organization
+{
+    public sealed class UserRemovedFromOrganizationDomainEvent : IDomainEvent
+    {
+        public Guid OrganizationId { get; }
+        public Guid UserId { get; }
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+        public Guid EventId { get; } = Guid.NewGuid();
+
+        public UserRemovedFromOrganizationDomainEvent(
+            Guid organizationId,
+            Guid userId)
+        {
+            OrganizationId = organizationId;
+            UserId = userId;
+        }
+    }
+}
