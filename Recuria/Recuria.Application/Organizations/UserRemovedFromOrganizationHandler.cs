aa656eca076b3f5367b2f01b83967fd324ecbaad
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-20 15:44:15 -0800

    UserRemovedFromOrganizationHandler created and implemented handleasync

---
+++ Recuria/Recuria.Application/Organizations/UserRemovedFromOrganizationHandler.cs
@@ -0,0 +1,41 @@
+﻿using Recuria.Application.Interface;
+using Recuria.Application.Interface.Abstractions;
+using Recuria.Domain.Events.Organization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Organizations
+{
+    public sealed class UserRemovedFromOrganizationHandler
+        : IDomainEventHandler<UserRemovedFromOrganizationDomainEvent>
+    {
+        private readonly IUserRepository _users;
+        private readonly IUnitOfWork _uow;
+
+        public UserRemovedFromOrganizationHandler(
+            IUserRepository users,
+            IUnitOfWork uow)
+        {
+            _users = users;
+            _uow = uow;
+        }
+
+        public async Task HandleAsync(
+            UserRemovedFromOrganizationDomainEvent @event,
+            CancellationToken ct)
+        {
+            var user = await _users.GetByIdAsync(@event.UserId, ct);
+            if (user == null)
+                return;
+
+            // Example future logic:
+            // user.RevokeAccess();
+            // user.DisableIfNoOrganizations();
+
+            await _uow.CommitAsync(ct);
+        }
+    }
+}
