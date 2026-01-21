2d2d80af130345fcd92b61b9038d8212735c41b4
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 11:45:13 -0800

    Added using imports for later use.

--- Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionActivationTests.cs
+++ Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionActivationTests.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Recuria.Application.Interface;
+using Recuria.Application.Requests;
+using Recuria.Domain.Enums;
+using Recuria.Infrastructure.Persistence;
+using Recuria.Infrastructure.Repositories;
+using Recuria.Tests.IntegrationTests.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace Recuria.Tests.IntegrationTests.Subscriptions
 {
-    public class SubscriptionActivationTests
+    public class SubscriptionActivationTests : IClassFixture<CustomWebApplicationFactory>
     {
     }
 }
