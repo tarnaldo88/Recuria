48cfdff6a82d7838591ad63c6087829261e369f9
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 09:58:57 -0800

    Created failing org created handler, modified Config web host method in customwebappfactory

--- Recuria/Recuria.Tests/IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
+++ Recuria/Recuria.Tests/IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -1,14 +1,18 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Recuria.Application.Interface;
+using Recuria.Domain.Events.Organization;
+using Recuria.Infrastructure.Persistence;
+using Recuria.Tests.IntegrationTests.TestDoubles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Recuria.Infrastructure.Persistence;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Recuria.Tests.IntegrationTests.Infrastructure
 {
@@ -18,18 +22,26 @@ namespace Recuria.Tests.IntegrationTests.Infrastructure
         {
             builder.ConfigureServices(services =>
             {
-                // Remove real DbContext
-                var descriptor = services.SingleOrDefault(
-                    d => d.ServiceType == typeof(DbContextOptions<RecuriaDbContext>));
+                //// Remove real DbContext
+                //var descriptor = services.SingleOrDefault(
+                //    d => d.ServiceType == typeof(DbContextOptions<RecuriaDbContext>));
 
-                if (descriptor != null)
-                    services.Remove(descriptor);
+                //if (descriptor != null)
+                //    services.Remove(descriptor);
 
-                // Add in-memory DB
-                services.AddDbContext<RecuriaDbContext>(options =>
-                {
-                    options.UseInMemoryDatabase("Recuria_TestDb");
-                });
+                //// Add in-memory DB
+                //services.AddDbContext<RecuriaDbContext>(options =>
+                //{
+                //    options.UseInMemoryDatabase("Recuria_TestDb");
+                //});
+
+                // Remove existing OrganizationCreated handlers
+                services.RemoveAll<IDomainEventHandler<OrganizationCreatedDomainEvent>>();
+
+                // Add failing one
+                services.AddScoped<
+                    IDomainEventHandler<OrganizationCreatedDomainEvent>,
+                    FailingOrganizationCreatedHandler>();
             });
         }
     }
---
+++ Recuria/Recuria.Tests/IntegrationTests/TestDoubles/FailingOrganizationCreatedHandler.cs
@@ -0,0 +1,21 @@
+﻿using Recuria.Application.Interface;
+using Recuria.Domain.Events.Organization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests.IntegrationTests.TestDoubles
+{
+    public sealed class FailingOrganizationCreatedHandler
+    : IDomainEventHandler<OrganizationCreatedDomainEvent>
+    {
+        public Task HandleAsync(
+            OrganizationCreatedDomainEvent domainEvent,
+            CancellationToken ct)
+        {
+            throw new InvalidOperationException("Simulated failure");
+        }
+    }
+}
