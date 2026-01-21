692d7dd8aea594741d09e5fea5a24086644a8d37
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 09:34:12 -0800

    Created CustomWebApplicationFactory and added mvc.testing package

---
+++ Recuria/Recuria.Tests/IntegrationTests/Infrastructure/CustomWebApplicationFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Recuria.Infrastructure.Persistence;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Recuria.Tests.IntegrationTests.Infrastructure
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // Remove real DbContext
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<RecuriaDbContext>));
+
+                if (descriptor != null)
+                    services.Remove(descriptor);
+
+                // Add in-memory DB
+                services.AddDbContext<RecuriaDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("Recuria_TestDb");
+                });
+            });
+        }
+    }
+}
