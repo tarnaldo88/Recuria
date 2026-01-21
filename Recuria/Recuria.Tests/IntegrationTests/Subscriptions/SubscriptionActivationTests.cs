b137364c35a57c9009dc88a0cef1ae78b22dd278
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 11:41:44 -0800

    Created activation and expiration test files. Next step to implement.

---
+++ Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionActivationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests.IntegrationTests.Subscriptions
+{
+    public class SubscriptionActivationTests
+    {
+    }
+}
