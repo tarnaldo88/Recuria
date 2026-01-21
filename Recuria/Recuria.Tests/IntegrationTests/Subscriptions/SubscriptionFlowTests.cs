9517012a68c6e8b104f3c697d841288104cdf153
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 09:43:27 -0800

    added SeedUser

--- Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionFlowTests.cs
+++ Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionFlowTests.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,14 @@ namespace Recuria.Tests.IntegrationTests.Subscriptions
     public class SubscriptionFlowTests : IntegrationTestBase
     {
         public SubscriptionFlowTests(CustomWebApplicationFactory factory) : base(factory) { }
+
+        private async Task SeedUser(Guid userId)
+        {
+            await Client.PostAsJsonAsync("/api/users", new
+            {
+                Id = userId,
+                Email = $"{userId}@test.com"
+            });
+        }
     }
 }
