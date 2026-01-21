8637404b39e3d9631949b28bcef482182067dfe4
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 12:40:11 -0800

    Helper method to make a persisted org and sub created.

--- Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionActivationTests.cs
+++ Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionActivationTests.cs
@@ -2,6 +2,8 @@
 using Recuria.Application.Interface;
 using Recuria.Application.Interface.Abstractions;
 using Recuria.Application.Requests;
+using Recuria.Domain;
+using Recuria.Domain.Entities;
 using Recuria.Domain.Enums;
 using Recuria.Infrastructure.Persistence;
 using Recuria.Infrastructure.Repositories;
@@ -31,6 +33,29 @@ namespace Recuria.Tests.IntegrationTests.Subscriptions
             _organizations = services.GetRequiredService<IOrganizationRepository>();
             _users = services.GetRequiredService<IUserRepository>();
             _uow = services.GetRequiredService<IUnitOfWork>();
+        }
+
+        //Creating helper method to make a persisted org and sub
+        private async Task<(Organization Org, Subscription Subscription)> CreateSubscriptionAsync(SubscriptionStatus status, DateTime periodStart, DateTime periodEnd)
+        {
+            var owner = new User($"{Guid.NewGuid()}@activationtest.com", "TestName");
+            await _users.AddAsync(owner, CancellationToken.None);
+
+            var org = new Organization($"Org-{Guid.NewGuid()}");
+            org.AddUser(owner, UserRole.Owner);
+            await _organizations.AddAsync(org, CancellationToken.None);
+
+            var subscription = new Subscription(
+                org,
+                PlanType.Pro,
+                status,
+                periodStart,
+                periodEnd);
+
+            await _subscriptions.AddAsync(subscription, CancellationToken.None);
+            await _uow.CommitAsync();
+
+            return (org, subscription);
         }
     }
 }
