b5eb668c4b41c2412d3ce2fb101cc28c9f8a77e1
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 09:34:59 -0800

    created integration test base

---
+++ Recuria/Recuria.Tests/IntegrationTests/Infrastructure/IntegrationTestBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using Xunit;
+
+namespace Recuria.Tests.IntegrationTests.Infrastructure
+{
+    public abstract class IntegrationTestBase
+    : IClassFixture<CustomWebApplicationFactory>
+    {
+        protected readonly HttpClient Client;
+
+        protected IntegrationTestBase(CustomWebApplicationFactory factory)
+        {
+            Client = factory.CreateClient();
+        }
+    }
+}
