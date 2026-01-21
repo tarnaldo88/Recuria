c38b888dfe51f6112d1abc50a3381327fdbc58ce
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-21 09:43:05 -0800

    created SubscriptionFlowTests and constructor

---
+++ Recuria/Recuria.Tests/IntegrationTests/Subscriptions/SubscriptionFlowTests.cs
@@ -0,0 +1,14 @@
+﻿using Recuria.Tests.IntegrationTests.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Tests.IntegrationTests.Subscriptions
+{
+    public class SubscriptionFlowTests : IntegrationTestBase
+    {
+        public SubscriptionFlowTests(CustomWebApplicationFactory factory) : base(factory) { }
+    }
+}
