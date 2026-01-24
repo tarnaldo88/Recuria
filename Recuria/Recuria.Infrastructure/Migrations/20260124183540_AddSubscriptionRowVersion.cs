3a030aa2088b139f0e69f003291a706ba520ddad
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-24 10:37:44 -0800

    Migrations applied successfully

---
+++ Recuria/Recuria.Infrastructure/Migrations/20260124183540_AddSubscriptionRowVersion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recuria.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSubscriptionRowVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrganizationId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "RowVersion",
+                table: "Subscriptions",
+                type: "rowversion",
+                rowVersion: true,
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RowVersion",
+                table: "Subscriptions");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "OrganizationId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
