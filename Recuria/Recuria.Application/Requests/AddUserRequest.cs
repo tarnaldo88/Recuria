d57c1c80bb8b654ed1c053de7d41df1b39655686
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-29 16:36:25 -0800

    adding xml comments

--- Recuria/Recuria.Application/Requests/AddUserRequest.cs
+++ Recuria/Recuria.Application/Requests/AddUserRequest.cs
@@ -8,11 +8,26 @@ using Recuria.Domain.Enums;
 
 namespace Recuria.Application.Requests
 {
+    /// <summary>
+    /// Request to add a user to an organization.
+    /// </summary>
     public class AddUserRequest
     {
+        /// <summary>
+        /// User display name.
+        /// </summary>
         public string? Name { get; init; }
+        /// <summary>
+        /// User email address (required if user does not exist).
+        /// </summary>
         public string? Email { get; init; }
+        /// <summary>
+        /// Role to assign.
+        /// </summary>
         public UserRole Role { get; init; }
+        /// <summary>
+        /// User id.
+        /// </summary>
         public Guid UserId { get; init; }
     }
 }
--- Recuria/Recuria.Application/Requests/ChangeUserRoleRequest.cs
+++ Recuria/Recuria.Application/Requests/ChangeUserRoleRequest.cs
@@ -8,8 +8,14 @@ using Recuria.Domain.Enums;
 
 namespace Recuria.Application.Requests
 {
+    /// <summary>
+    /// Request to change a user's role.
+    /// </summary>
     public class ChangeUserRoleRequest
     {
+        /// <summary>
+        /// New role for the user.
+        /// </summary>
         public UserRole NewRole { get; set; }
     }
 }
