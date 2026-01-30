7208550de519ce63a847294a2daf946e9be57e3f
tarnaldo88 <tarnaldo88@gmail.com>
2026-01-29 16:35:52 -0800

    adding xml comments

--- Recuria/Recuria.Application/Requests/CreateOrganizationRequest.cs
+++ Recuria/Recuria.Application/Requests/CreateOrganizationRequest.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace Recuria.Application.Requests
 {
+    /// <summary>
+    /// Request to create an organization.
+    /// </summary>
     public class CreateOrganizationRequest
     {
+        /// <summary>
+        /// Organization name.
+        /// </summary>
         public string Name { get; init; } = string.Empty;
+        /// <summary>
+        /// Owner user id.
+        /// </summary>
         public Guid OwnerId { get; init; }
     }
 }
