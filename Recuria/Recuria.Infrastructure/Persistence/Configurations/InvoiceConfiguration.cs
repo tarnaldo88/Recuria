e12a0a87893d4fd40929e6c43582270bef8d18f7
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-02 10:01:35 -0800

    Adding invoice checks

--- Recuria/Recuria.Infrastructure/Persistence/Configurations/InvoiceConfiguration.cs
+++ Recuria/Recuria.Infrastructure/Persistence/Configurations/InvoiceConfiguration.cs
@@ -15,12 +15,18 @@ namespace Recuria.Infrastructure.Persistence.Configurations
         {
             builder.HasKey(i => i.Id);
 
+            builder.Property(i => i.InvoiceNumber)
+                .HasMaxLength(64);
+
             builder.Property(i => i.Amount)
                 .HasPrecision(18, 2) //Important for money
                 .IsRequired();
 
             builder.Property(i => i.InvoiceDate)
                 .IsRequired();
+
+            builder.HasIndex(i => i.SubscriptionId);
+            builder.HasIndex(i => i.InvoiceDate);
         }
     }
 }
