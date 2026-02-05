be078f914ff608520c3ee1ab8a7c69383f461b22
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-05 13:16:10 -0800

    added project reference and scope

--- Recuria/Recuira.Blazor/Program.cs
+++ Recuria/Recuira.Blazor/Program.cs
@@ -6,6 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp =>
+{
+    var http = new HttpClient { BaseAddress = new Uri("http://localhost:5132") };
+    return new Recuria.Client.RecuriaApiClient(http);
+});
 
 await builder.Build().RunAsync();
