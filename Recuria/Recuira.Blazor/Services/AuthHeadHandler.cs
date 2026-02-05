cb8626af050869ff102043809ceef46ef80c2433
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-05 14:46:24 -0800

    Added Auth head handler

---
+++ Recuria/Recuira.Blazor/Services/AuthHeadHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Headers;
+
+namespace Recuira.Blazor.Services
+{
+    public sealed class AuthHeadHandler : DelegatingHandler
+    {
+        private readonly AuthState _auth;
+
+        public AuthHeadHandler(AuthState auth)
+        {
+            _auth = auth;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _auth.GetTokenAsync();
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token); 
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
