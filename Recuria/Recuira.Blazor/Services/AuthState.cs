74f180d9b96ec389871d2193a7b42b13f3672f41
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-05 13:36:35 -0800

    implementation complete. Get async, and set async completed.

--- Recuria/Recuira.Blazor/Services/AuthState.cs
+++ Recuria/Recuira.Blazor/Services/AuthState.cs
@@ -1,7 +1,25 @@
 ﻿namespace Recuira.Blazor.Services
 {
     //token + org id + role
-    public class AuthState
+    public sealed class AuthState
     {
+        private readonly TokenStorage _storage;
+
+        public AuthState(TokenStorage storage) => _storage = storage;
+
+        public async Task<string?> GetTokenAsync() => await _storage.GetTokenAsync();
+        public async Task<string?> GetOrgIdAsync() => await _storage.GetOrgIdAsync();
+
+        public async Task SetAuthAsync(string token, string orgId)
+        {
+            await _storage.SetTokenAsync(token);
+            await _storage.SetOrgIdAsync(orgId);
+        }
+
+        public async Task ClearAsync()
+        {
+            await _storage.ClearTokenAsync();
+            await _storage.ClearOrgIdAsync();
+        }
     }
 }
