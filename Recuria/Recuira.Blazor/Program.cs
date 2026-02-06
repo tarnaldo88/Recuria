58b4d1d124cde0b247c4da565da84dab4e138f95
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-06 12:31:45 -0800

    adding mud blazor

--- Recuria/Recuira.Blazor/Program.cs
+++ Recuria/Recuira.Blazor/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Recuira.Blazor;
 using Recuira.Blazor.Services;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
