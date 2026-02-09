85b1da5419ca6e2dc55f20427b66868b0a7e0f67
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-09 12:35:26 -0800

    created dto

---
+++ Recuria/Recuria.Application/Contracts/Organizations/UserSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿using Recuria.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts.Organizations
+{
+    public sealed record UserSummaryDto(
+        Guid Id,
+        string? Name,
+        string? Email,
+        UserRole Role,
+        Guid? OrganizationId
+    );
+}
