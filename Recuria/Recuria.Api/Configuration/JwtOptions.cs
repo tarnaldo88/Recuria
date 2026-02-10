f34f1facde39dcfe4f702e7b9af7391ad3352656
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-10 11:53:26 -0800

    added time for token

--- Recuria/Recuria.Api/Configuration/JwtOptions.cs
+++ Recuria/Recuria.Api/Configuration/JwtOptions.cs
@@ -7,5 +7,6 @@ namespace Recuria.Api.Configuration
         public string Issuer { get; init; } = string.Empty;
         public string Audience { get; init; } = string.Empty;
         public string SigningKey { get; init; } = string.Empty;
+        public int AccessTokenMinutes { get; init; } = 60;
     }
 }
