4627cf7b194739fae7fa13d0b530238a1d584c8e
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-11 11:05:34 -0800

    added blazor app services

--- Recuria/Recuria.Blazor/Program.cs
+++ Recuria/Recuria.Blazor/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Recuria.Blazor;
 using Recuria.Blazor.Services;
+using Recuria.Blazor.Services.App;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services; 
 
@@ -12,6 +13,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped<AuthHeaderHandler>();
 builder.Services.AddScoped<TokenStorage>();
 builder.Services.AddScoped<AuthState>();
+builder.Services.AddScoped<ApiCallRunner>();
+builder.Services.AddScoped<IAuthAppService, AuthAppService>();
+builder.Services.AddScoped<IBootstrapAppService, BootstrapAppService>();
+builder.Services.AddScoped<IOrganizationAppService, OrganizationAppService>();
+builder.Services.AddScoped<ISubscriptionAppService, SubscriptionAppService>();
+builder.Services.AddScoped<IInvoiceAppService, InvoiceAppService>();
+builder.Services.AddScoped<IUserAppService, UserAppService>();
+builder.Services.AddScoped<IOpsAppService, OpsAppService>();
 builder.Services.AddMudServices();
 
 var apiBaseUrl = builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5132/";
