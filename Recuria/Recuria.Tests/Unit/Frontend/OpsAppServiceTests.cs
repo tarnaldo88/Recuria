e4d107a9f12c4a5f9cda7a1064e00867238456f3
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-12 13:36:58 -0800

    created ops service tests

---
+++ Recuria/Recuria.Tests/Unit/Frontend/OpsAppServiceTests.cs
@@ -0,0 +1,67 @@
+using FluentAssertions;
+using Moq;
+using MudBlazor;
+using Recuria.Blazor.Services.App;
+
+namespace Recuria.Tests.Unit.Frontend
+{
+    public sealed class OpsAppServiceTests
+    {
+        private readonly Mock<Recuria.Client.IRecuriaApiClient> _api = new();
+        private readonly ApiCallRunner _runner;
+
+        public OpsAppServiceTests()
+        {
+            var snackbar = new Mock<ISnackbar>();
+            _runner = new ApiCallRunner(snackbar.Object);
+        }
+
+        [Fact]
+        public async Task RetryAsync_Should_Treat_204_Exception_As_Success()
+        {
+            _api.Setup(x => x.RetryAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(CreateApiException(204, string.Empty));
+
+            var service = new OpsAppService(_api.Object, _runner);
+            var result = await service.RetryAsync(Guid.NewGuid());
+
+            result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetDeadLetteredAsync_Should_Return_Data_When_Api_Succeeds()
+        {
+            var items = new List<Recuria.Client.DeadLetteredOutboxItem>
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Type = "SubscriptionActivated",
+                    Error = "None",
+                    RetryCount = 1,
+                    DeadLetteredOnUtc = DateTime.UtcNow
+                }
+            };
+
+            _api.Setup(x => x.DeadLetteredAsync(It.IsAny<int?>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(items);
+
+            var service = new OpsAppService(_api.Object, _runner);
+            var result = await service.GetDeadLetteredAsync(50);
+
+            result.Success.Should().BeTrue();
+            result.Data.Should().NotBeNull();
+            result.Data!.Count.Should().Be(1);
+        }
+
+        private static Recuria.Client.ApiException CreateApiException(int statusCode, string response)
+        {
+            return new Recuria.Client.ApiException(
+                message: "API Error",
+                statusCode: statusCode,
+                response: response,
+                headers: new Dictionary<string, IEnumerable<string>>(),
+                innerException: null);
+        }
+    }
+}
