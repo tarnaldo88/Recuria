7bce9f095ccf3a99b280a2ee08590a30acfa2281
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-13 08:50:58 -0800

    created interface and record

---
+++ Recuria/Recuria.Application/Interface/Idempotency/IApiIdempotencyStore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Interface.Idempotency
+{
+    public sealed record ApiIdempotencyHit(Guid ResourceId, string RequestHash);
+
+    public interface IApiIdempotencyStore
+    {
+        Task<ApiIdempotencyHit?> GetAsync(Guid organizationId, string operation, string key, CancellationToken ct);
+
+        Task SaveAsync(
+            Guid organizationId,
+            string operation,
+            string key,
+            string requestHash,
+            Guid resourceId,
+            CancellationToken ct);
+    }
+}
