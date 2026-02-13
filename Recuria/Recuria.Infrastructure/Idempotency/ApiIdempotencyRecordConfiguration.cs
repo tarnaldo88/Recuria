3cd78d5257abb909035d5169bfa6ac49196d2779
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-13 08:48:12 -0800

    file created, interface implemented

---
+++ Recuria/Recuria.Infrastructure/Idempotency/ApiIdempotencyRecordConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Recuria.Infrastructure.Persistence.Entities;
+
+namespace Recuria.Infrastructure.Idempotency
+{
+    internal sealed class ApiIdempotencyRecordConfiguration : IEntityTypeConfiguration<ApiIdempotencyRecord>
+    {
+        public void Configure(EntityTypeBuilder<ApiIdempotencyRecord> builder)
+        {
+            builder.ToTable("ApiIdempotencyRecords");
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Operation).HasMaxLength(100).IsRequired();
+            builder.Property(x => x.IdempotencyKey).HasMaxLength(120).IsRequired();
+            builder.Property(x => x.RequestHash).HasMaxLength(128).IsRequired();
+            builder.Property(x => x.CreatedOnUtc).IsRequired();
+
+            builder.HasIndex(x => new { x.OrganizationId, x.Operation, x.IdempotencyKey })
+                .IsUnique();
+        }
+    }
+}
