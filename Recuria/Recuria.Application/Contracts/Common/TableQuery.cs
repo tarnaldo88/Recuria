2e8c0ed7e09e51ae96907354181abb3e2decafdb
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-14 16:33:32 -0800

    table query class created

---
+++ Recuria/Recuria.Application/Contracts/Common/TableQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Application.Contracts.Common
+{
+    public sealed class TableQuery
+    {
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+        public string? Search { get; init; }
+        public string? SortBy { get; init; }
+        public string SortDir { get; init; } = "asc";
+    }
+}
