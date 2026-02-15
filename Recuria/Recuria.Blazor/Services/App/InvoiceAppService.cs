e7a2c530446a1b9ba71f2147dd79e547d00e12a4
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-15 11:02:44 -0800

    deleted duplicate method

--- Recuria/Recuria.Blazor/Services/App/InvoiceAppService.cs
+++ Recuria/Recuria.Blazor/Services/App/InvoiceAppService.cs
@@ -28,15 +28,7 @@ namespace Recuria.Blazor.Services.App
         }
 
         public Task<AppResult<ICollection<Recuria.Client.InvoiceListItemDto>>> GetByOrganizationAsync(Guid organizationId, bool notifyError = true) =>
-            _runner.RunAsync(() => _api.OrganizationAsync(organizationId), errorPrefix: "Unable to load invoices", notifyError: notifyError);
-
-        public Task<AppResult<Guid>> CreateAsync(Recuria.Client.CreateInvoiceRequest request, bool notifySuccess = true) =>
-            _runner.RunAsync(
-                () => _api.InvoicesPOSTAsync(request),
-                successMessage: "Invoice created.",
-                errorPrefix: "Unable to create invoice",
-                notifySuccess: notifySuccess,
-                notifyError: true);
+            _runner.RunAsync(() => _api.OrganizationAsync(organizationId), errorPrefix: "Unable to load invoices", notifyError: notifyError);        
 
         public Task<AppResult<Recuria.Client.InvoiceDetailsDto>> GetByIdAsync(Guid invoiceId, bool notifyError = true) =>
             _runner.RunAsync(() => _api.InvoicesGETAsync(invoiceId), errorPrefix: "Unable to load invoice", notifyError: notifyError);
