14672629f27d79afe07325fb466b0526988f4ad8
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-16 11:15:54 -0800

    created test file and added constructor

---
+++ Recuria/Recuria.Tests/Unit/Frontend/InvoicesServerTableBehaviorTests.cs
@@ -0,0 +1,21 @@
+﻿using Bunit;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+using Moq;
+using MudBlazor.Services;
+using Recuria.Blazor.Pages;
+using Recuria.Blazor.Services;
+using Recuria.Blazor.Services.App;
+
+namespace Recuria.Tests.Unit.Frontend
+{
+    public sealed class InvoicesServerTableBehaviorTests : TestContext
+    {
+        public InvoicesServerTableBehaviorTests()
+        {
+            JSInterop.Mode = JSRuntimeMode.Loose;
+            Services.AddMudServices();
+        }
+    }
+}
