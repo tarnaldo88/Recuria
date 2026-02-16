4703f4cc7a8d25bffe8a3ba3d5a1895ddf556f71
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-16 11:17:53 -0800

    created test file and constructor

---
+++ Recuria/Recuria.Tests/Unit/Frontend/OpsServerTableBehaviorTests.cs
@@ -0,0 +1,18 @@
+﻿using Bunit;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using MudBlazor.Services;
+using Recuria.Blazor.Pages;
+using Recuria.Blazor.Services.App;
+
+namespace Recuria.Tests.Unit.Frontend
+{
+    public sealed class OpsServerTableBehaviorTests : TestContext
+    {
+        public OpsServerTableBehaviorTests()
+        {
+            Services.AddMudServices();
+        }
+    }
+}
