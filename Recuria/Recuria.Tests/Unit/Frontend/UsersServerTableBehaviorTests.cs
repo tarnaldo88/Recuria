7646a580205f2929da2e2922790fb5985501cb62
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-16 11:14:17 -0800

    Added test methods

--- Recuria/Recuria.Tests/Unit/Frontend/UsersServerTableBehaviorTests.cs
+++ Recuria/Recuria.Tests/Unit/Frontend/UsersServerTableBehaviorTests.cs
@@ -38,5 +38,65 @@ namespace Recuria.Tests.Unit.Frontend
             cut.WaitForAssertion(() => cut.Markup.Should().Contain("No users found."));
 
         }
+
+        [Fact]
+        public void UsersTable_Should_Show_Error_State()
+        {
+            var orgId = Guid.NewGuid();
+            var usersApi = BuildUsersApi(orgId, AppResult<Recuria.Client.UserSummaryDtoPagedResult>.Fail("Unable to load users."));
+
+            RegisterUsersPageServices(orgId, usersApi.Object);
+
+            var cut = RenderComponent<Users>();
+
+            cut.WaitForAssertion(() => cut.Markup.Should().Contain("Unable to load users."));
+        }
+
+        [Fact]
+        public void UsersTable_Should_Transition_From_Loading_To_Empty()
+        {
+            var orgId = Guid.NewGuid();
+            var tcs = new TaskCompletionSource<AppResult<Recuria.Client.UserSummaryDtoPagedResult>>();
+            var usersApi = new Mock<IUserAppService>();
+
+            usersApi.Setup(x => x.GetPageAsync(
+                    orgId,
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<bool>()))
+                .Returns(() => tcs.Task);
+
+            RegisterUsersPageServices(orgId, usersApi.Object);
+
+            var cut = RenderComponent<Users>();
+
+            cut.WaitForAssertion(() =>
+                usersApi.Verify(x => x.GetPageAsync(
+                    orgId,
+                    It.IsAny<int>(),
+                    It.IsAny<int>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<string?>(),
+                    It.IsAny<bool>()), Times.Once));
+
+            cut.Markup.Should().NotContain("No users found.");
+
+            tcs.SetResult(AppResult<Recuria.Client.UserSummaryDtoPagedResult>.Ok(
+                new Recuria.Client.UserSummaryDtoPagedResult
+                {
+                    Items = Array.Empty<Recuria.Client.UserSummaryDto>(),
+                    TotalCount = 0,
+                    Page = 1,
+                    PageSize = 10
+                }));
+
+            cut.WaitForAssertion(() => cut.Markup.Should().Contain("No users found."));
+        }
+
+
     }
 }
