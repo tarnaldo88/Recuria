55102c72c32946360be1838e17d49855795b0436
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 10:59:04 -0800

    added stripe options

---
+++ Recuria/Recuria.Api/Configuration/StripeOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Recuria.Api.Configuration
+{
+    public sealed class StripeOptions
+    {
+        public const string SectionName = "Stripe";
+        public string SecretKey { get; init; }
+        public string WebhookSecret { get; init; }
+        public string SuccessUrl { get; init; }
+        public string CancelUrl { get; init; }
+    }
+}
