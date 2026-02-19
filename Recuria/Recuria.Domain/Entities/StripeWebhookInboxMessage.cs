f2f128051b10d692ef7bee196c87aaedb08fd3f3
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 12:16:31 -0800

    created file

---
+++ Recuria/Recuria.Domain/Entities/StripeWebhookInboxMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Entities
+{
+    public sealed class StripeWebhookInboxMessage
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string StripeEventId { get; set; } = string.Empty;
+        public string EventType { get; set; } = string.Empty;
+        public string Payload { get; set; } = string.Empty;
+
+        public DateTime ReceivedOnUtc { get; set; } = DateTime.UtcNow;
+        public DateTime? ProcessedOnUtc { get; set; }
+
+        public int AttemptCount { get; set; }
+        public DateTime? NextAttemptOnUtc { get; set; }
+        public string? LastError { get; set; }
+    }
+}
