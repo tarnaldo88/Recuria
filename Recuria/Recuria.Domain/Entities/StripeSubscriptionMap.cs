c260f85add54270bec9dd279ce9bb83dd6935364
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 11:26:40 -0800

    created file

---
+++ Recuria/Recuria.Domain/Entities/StripeSubscriptionMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuria.Domain.Entities
+{
+    public sealed class StripeSubscriptionMap
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid OrganizationId { get; set; }
+
+        public string StripeCustomerId { get; set; } = string.Empty;
+        public string StripeSubscriptionId { get; set; } = string.Empty;
+
+        public DateTime CreatedOnUtc { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedOnUtc { get; set; } = DateTime.UtcNow;
+    }
+}
