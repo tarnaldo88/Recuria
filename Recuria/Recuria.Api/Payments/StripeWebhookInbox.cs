6fac6d8143a990a5b61aac65b0d6ddd17e8dcdf6
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 12:22:26 -0800

    created file and interface

---
+++ Recuria/Recuria.Api/Payments/StripeWebhookInbox.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recuria.Domain.Entities;
+using Recuria.Infrastructure.Persistence;
+using Stripe;
+
+namespace Recuria.Api.Payments
+{
+    public interface IStripeWebhookInbox
+    {
+        Task EnqueueAsync(string eventId, string eventType, string payload, CancellationToken ct);
+        Task<List<StripeWebhookInboxMessage>> GetPendingAsync(int take, CancellationToken ct);
+        Task MarkProcessedAsync(Guid id, CancellationToken ct);
+        Task MarkFailedAsync(Guid id, string error, CancellationToken ct);
+    }
+
+    public class StripeWebhookInbox
+    {
+    }
+}
