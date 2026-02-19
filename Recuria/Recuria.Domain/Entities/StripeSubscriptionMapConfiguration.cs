9a15ec7f869920030a868fe255079b6a8a697764
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 11:27:43 -0800

    created class

---
+++ Recuria/Recuria.Domain/Entities/StripeSubscriptionMapConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Recuria.Domain.Entities;
+
+namespace Recuria.Domain.Entities
+{
+    public sealed class StripeSubscriptionMapConfiguration : IEntityTypeConfiguration<StripeSubscriptionMap>
+    {
+        public void Configure(EntityTypeBuilder<StripeSubscriptionMap> b)
+        {
+            b.ToTable("StripeSubscriptionMaps");
+            b.HasKey(x => x.Id);
+
+            b.Property(x => x.StripeCustomerId).HasMaxLength(128).IsRequired();
+            b.Property(x => x.StripeSubscriptionId).HasMaxLength(128).IsRequired();
+
+            b.HasIndex(x => x.OrganizationId).IsUnique();
+            b.HasIndex(x => x.StripeCustomerId);
+            b.HasIndex(x => x.StripeSubscriptionId);
+        }
+    }
+}
