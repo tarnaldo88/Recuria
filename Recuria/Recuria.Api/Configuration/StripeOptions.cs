8aadc245e6b40180a42ab55bca819b9af4a2a5d8
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-19 11:03:29 -0800

    created file, members and constructor

--- Recuria/Recuria.Api/Configuration/StripeOptions.cs
+++ Recuria/Recuria.Api/Configuration/StripeOptions.cs
@@ -3,9 +3,9 @@
     public sealed class StripeOptions
     {
         public const string SectionName = "Stripe";
-        public string SecretKey { get; init; }
-        public string WebhookSecret { get; init; }
-        public string SuccessUrl { get; init; }
-        public string CancelUrl { get; init; }
+        public string SecretKey { get; init; } = "";
+        public string WebhookSecret { get; init; } = "";
+        public string SuccessUrl { get; init; } = "";
+        public string CancelUrl { get; init; } = "";
     }
 }
---
+++ Recuria/Recuria.Api/Controllers/PaymentsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Recuria.Api.Configuration;
+using Stripe;
+using Stripe.Checkout;
+
+namespace Recuria.Api.Controllers
+{
+    [ApiController]
+    [Route("api/payments")]
+    public sealed class PaymentsController : ControllerBase
+    {
+        private readonly SessionService _sessions;
+        private readonly StripeOptions _stripe;
+
+        public PaymentsController(SessionService sessions, StripeOptions stripe)
+        {
+            _sessions = sessions;
+            _stripe = stripe;
+        }
+
+
+    }
+}
