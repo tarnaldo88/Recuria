eaf5ebfb5aa4ebe1647a67d4659d9f9ac3ed3696
tarnaldo88 <tarnaldo88@gmail.com>
2026-02-25 21:58:58 -0800

    forgot to add auth api using statement

--- Recuria/Recuria.Api/Controllers/OutboxController.cs
+++ Recuria/Recuria.Api/Controllers/OutboxController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Recuria.Api.Auth;
 using Recuria.Infrastructure.Outbox;
 using Recuria.Infrastructure.Persistence;
 using Recuria.Application.Interface.Idempotency;
